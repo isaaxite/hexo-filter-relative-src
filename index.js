25dfb4d9a383f5b238ec986a57921bd16d74ea08
isaaxite <isaacgun@outlook.com>
2023-06-02 10:48:06 +0800

    feat: delete redundant code

--- index.js
+++ index.js
@@ -3,9 +3,20 @@
 'use strict';
 
 const filter = require('./lib/filter');
+const log = require('hexo-log').default({
+  debug: false,
+  silent: false
+});
+
+log.info('Hello HexoRelativedImg!')
 
 hexo.extend.filter.register('marked:renderer', function(renderer) {
   const { relatived_img } = hexo.config;
-  const isEnable = relatived_img === 'undefined' ? true : relatived_img;
-  return isEnable ? filter.call(this, renderer) : renderer;
+  const isEnable = typeof relatived_img === 'undefined' ? true : relatived_img;
+  console.info(11111, relatived_img, isEnable)
+  if (isEnable) {
+    console.info(1111)
+    return filter.call(this, renderer);
+  }
+  return renderer;
 });
