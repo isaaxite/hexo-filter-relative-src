e9af80482ff03cf868007a3f2114750bd1f3b0ae
isaaxite <isaacgun@outlook.com>
2023-07-10 22:16:14 +0800

    fix: parse permalink

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 'use strict';
 
 const assign = require('object-assign');
-const filter = require('./lib/filter');
+const { parse } = require('node-html-parser');
+const path = require('path');
+const { URL } = require('url');
 // const log = require('hexo-log').default({
 //   debug: false,
 //   silent: false
@@ -20,10 +22,43 @@ hexo.config.relitive_src_filter = assign({
   enable: typeof relitive_src_filter.enable === 'undefined' ? true : relitive_src_filter.enable
 }, hexo.config.relitive_src_filter);
 
-hexo.extend.filter.register('marked:renderer', function(renderer) {
-  // log.info('relitive_src_filter conf:', relitive_src_filter);
-  if (relitive_src_filter.enable) {
-    return filter.call(this, renderer);
+hexo.extend.filter.register('after_post_render', function(data) {
+  console.info(relitive_src_filter)
+  if (!relitive_src_filter.enable) {
+    return data;
   }
-  return renderer;
+
+  const document = parse(data.content);
+  const imgEles = document.querySelectorAll('img');
+  const anchorEles = document.querySelectorAll('a');
+
+  const isUrl = (link) => /^(#|\/\/|http(s)?:)/.test(link)
+  const getFormatLink = (link) => {
+    let lastLink = link;
+    if (isUrl(link)) {
+      return lastLink;
+    }
+
+    lastLink = path.basename(lastLink);
+
+    if (relitive_src_filter.prefix && isUrl(relitive_src_filter.prefix)) {
+      lastLink = new URL(lastLink, relitive_src_filter.prefix);
+    }
+    return lastLink;
+  };
+
+  const batchFormatLink = (eles, attrName) => {
+    for (const ele of eles) {
+      const attr = ele.getAttribute(attrName);
+      const lastAttr = getFormatLink(attr);
+      ele.setAttribute(attrName, lastAttr);
+    }
+  };
+
+  batchFormatLink(imgEles, 'src');
+  batchFormatLink(anchorEles, 'href');
+
+  data.content = document.toString();
+
+  return data
 });
